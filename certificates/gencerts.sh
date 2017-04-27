4cf613dffb0cdea05628ac312cec8b0357ba02a4
zsiegel <zsiegel87@gmail.com>
2017-04-27 17:31:43 -0500

    updating certificate info

--- certificates/gencerts.sh
+++ certificates/gencerts.sh
@@ -3,7 +3,3 @@
 openssl req \
         -newkey rsa:2048 -nodes -keyout tls.key \
         -x509 -days 365 -out tls.crt
-
-kubectl create secret generic zcorp-https \
-        --from-file=../certificates/tls.crt \
-        --from-file=../certificates/tls.key
